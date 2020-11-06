7b8d159b9e05095d2d5dedd9d7d50833d9048cf0
ttung288 <tung2808tung@gmail.com>
2020-11-06 09:15:32 +0700

    Testing_System_Assignment_4

--- SQL/Asignment4/Testing_System_Assignment_4.sql
+++ SQL/Asignment4/Testing_System_Assignment_4.sql
@@ -268,13 +268,14 @@ VALUES						(1			, 10			),
                             (3			, 8				),
                             (4			, 7				),
                             (5			, 6				),
-                            (6			, 5				),
+                            (6			, 10			),
                             (7			, 4				),
                             (8			, 3				),
                             (9			, 2				),
                             (10			, 1				);
                             
 -- Assignment 4
+/*======Exercise1========*/
 -- Question 1: Viết lệnh để lấy ra danh sách nhân viên và thông tin phòng ban của họ
 SELECT  *
 FROM `account` a 
@@ -298,13 +299,13 @@ LEFT JOIN `account` a  ON d.department_id = a.department_id
 GROUP BY d.department_id HAVING COUNT(*) >= 2;
 
 -- Question 5: Viết lệnh để lấy ra danh sách câu hỏi được sử dụng trong đề thi nhiều nhất
-SELECT *,COUNT(*)
+SELECT *,COUNT(*),GROUP_CONCAT(exam_id)
 FROM question q
 JOIN exam_question eq ON q.question_id = eq.question_id
 GROUP BY eq.question_id
 ORDER BY COUNT(*) DESC
 LIMIT 1;
-/*======Exercise1========*/
+
 -- Question 6: Thông kê mỗi category Question được sử dụng trong bao nhiêu Question
 SELECT *
 FROM category_question cq
@@ -324,48 +325,36 @@ LEFT JOIN answer a ON q.question_id = a.question_id
 GROUP BY q.question_id ORDER BY COUNT(a.answer_id) DESC;
 
 -- Question 9: Thống kê số lượng account trong mỗi group
-SELECT *,COUNT(ga.group_id) AS soluongaccount
-FROM `account` a 
-LEFT JOIN group_account ga ON ga.account_id = a.account_id
-GROUP BY a.account_id ;
+SELECT COUNT(account_id), group_id, group_name
+FROM group_account
+RIGHT JOIN `group` USING (group_id)
+GROUP BY group_id;
  
 -- Question 10: Tìm chức vụ có ít người nhất
 SELECT *, COUNT(a.account_id) AS songuoi
 FROM position p
 LEFT JOIN `account` a ON a.position_id = p.position_id
-GROUP BY p.position_id ORDER BY COUNT(a.account_id) DESC ;
+GROUP BY p.position_id
+HAVING COUNT(a.account_id) = 2;
 
--- Question 11: Thống kê mỗi phòng ban có bao nhiêu dev, test, scrum master, PM
-SELECT *,COUNT(p.position_name) AS songuoi
-FROM `account` a 
-JOIN position p ON p.position_id = a.position_id
-WHERE p.position_name = 'Dev'
 
-UNION
-
-SELECT *,COUNT(p.position_name) 
-FROM `account` a 
-JOIN position p ON p.position_id = a.position_id
-WHERE p.position_name = 'Test'
-
-UNION
-
-SELECT *,COUNT(p.position_name) 
-FROM `account` a 
-JOIN position p ON p.position_id = a.position_id
-WHERE p.position_name = 'Scrum Master'
-
-UNION
-
-SELECT *,COUNT(p.position_name) 
-FROM `account` a 
-JOIN position p ON p.position_id = a.position_id
-WHERE p.position_name = 'PM';
+-- Question 11: Thống kê mỗi phòng ban có bao nhiêu nhân viên 
+-- thuộc 4 chức vụ dev, test, scrum master, PM
+SELECT *,COUNT(department_id)
+FROM `account`
+JOIN position USING (position_id)
+WHERE position_name IN ('Dev','Test','Scrum Master','PM') 
+GROUP BY department_id 
+;
 
 -- Question 12: Lấy thông tin chi tiết của câu hỏi bao gồm: 
 -- thông tin cơ bản của question, loại câu hỏi, ai là người tạo ra câu hỏi, câu trả lời là gì, ...
-
-
+SELECT *
+FROM question q
+LEFT JOIN type_question tq ON q.type_id = tq.type_id
+LEFT JOIN `account` ac ON ac.account_id = q.creator_id
+LEFT JOIN answer a ON q.question_id = a.question_id
+;
 -- Question 13: Lấy ra số lượng câu hỏi của mỗi loại tự luận hay trắc nghiệm
 SELECT *, COUNT(q.question_id)
 FROM type_question tq
@@ -394,3 +383,16 @@ JOIN group_account ga ON ga.account_id = a.account_id
 WHERE ga.group_id = 2;
 
 -- Question 18:
+SELECT g.group_id, g.group_name, COUNT(gc.account_id) AS SoluongAccount
+FROM `group` g
+JOIN group_account gc ON gc.group_id = g.group_id
+GROUP BY g.group_id
+HAVING COUNT(gc.account_id) > 5
+
+UNION ALL
+
+SELECT  g.group_id, g.group_name, COUNT(gc.account_id) AS SoluongAccount
+FROM `group` g
+JOIN group_account gc ON gc.group_id = g.group_id
+GROUP BY g.group_id
+HAVING COUNT(gc.account_id) > 7;
